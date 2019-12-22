138d296f3def795f5525de8f42bb728b2ab412f2
Vu Huy <vuhuycto@gmail.com>
2019-12-22 10:46:08 +0700

    add gitignore

---
+++ app.py
@@ -0,0 +1,49 @@
+import os
+import zipfile
+
+from flask import Flask, render_template, request, send_file, jsonify
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+root = os.getcwd() + "/storage"
+os.chdir(root)
+
+
+@app.route("/")
+def index():
+    return render_template('file.html')
+
+
+@app.route("/<string:username>/<int:product_id>", methods=["GET", "POST"])
+def upload_image(username, product_id):
+    username_cwd = root + "/" + username
+    if username not in os.listdir(root):
+        os.mkdir(username_cwd)
+
+    os.chdir(username_cwd)
+    product_cwd = username_cwd + "/" + str(product_id)
+    if str(product_id) not in os.listdir(username_cwd):
+        os.mkdir(product_cwd)
+
+    os.chdir(product_cwd)
+
+    try:
+        if request.method == "POST":
+            for f in request.files.getlist("images"):
+                f.save(secure_filename(f.filename))
+            return jsonify({"message": "Upload images successfully"}), 201
+        elif request.method == "GET":
+            with zipfile.ZipFile("images.zip", "w") as images_zip:
+                for image in os.listdir(product_cwd):
+                    if os.path.isfile(image) and image.endswith(".jpg"):
+                        images_zip.write(image)
+            return send_file(product_cwd + "/images.zip", attachment_filename="images.zip", as_attachment=True)
+    except Exception as e:
+        print(e)
+        return jsonify({"message": "An internal error has occurred"}), 500
+    finally:
+        os.chdir(root)
+
+
+if __name__ == '__main__':
+    app.run(host="192.168.2.107", port=5000)
