b01aa6c56839652d4d0690d4f61b5171c67862dd
Vu Huy <vuhuycto@gmail.com>
2019-12-25 19:34:13 +0700

    Add runtime and requirement and procfile

--- app.py
+++ app.py
@@ -45,5 +45,31 @@ def upload_image(username, product_id):
         os.chdir(root)
 
 
+@app.route("/<string:username>/avatar")
+def avatar(username):
+    username_cwd = root + "/" + username
+    if username not in os.listdir(root):
+        os.mkdir(username_cwd)
+
+    os.chdir(username_cwd)
+    avatar_cwd = username_cwd + "/avatar"
+    if "avatar" not in os.listdir(username_cwd):
+        os.mkdir(avatar_cwd)
+
+    os.chdir(avatar_cwd)
+    try:
+        if request.method == "POST":
+            for f in request.files["avatar"]:
+                f.save(secure_filename(f.filename))
+            return jsonify({"message": "Upload images successfully"}), 201
+        elif request.method == "GET":
+            return send_file(avatar_cwd + "/avatar.jpg", attachment_filename="avatar.jpg", as_attachment=True)
+    except Exception as e:
+        print(e)
+        return jsonify({"message": "An internal error has occurred"}), 500
+    finally:
+        os.chdir(root)
+
+
 if __name__ == '__main__':
     app.run(host="192.168.2.107", port=5000)
